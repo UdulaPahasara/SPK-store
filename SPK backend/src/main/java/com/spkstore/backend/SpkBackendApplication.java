9972b1133ae185ffcff9fbd8a5b9fee68161f8de
UdulaPahasara <udulapahasara2@gmail.com>
2026-04-29 10:41:04 -0700

    main class

---
+++ SPK backend/src/main/java/com/spkstore/backend/SpkBackendApplication.java
@@ -0,0 +1,12 @@
+package com.spkstore.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpkBackendApplication.class, args);
+    }
+}
